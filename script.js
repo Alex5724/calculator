664a42299a4c566d0d98e9aa7d65de3368555083
Alex <221934805+Alex5724@users.noreply.github.com>
2025-09-01 20:19:51 +0300

    The real final version of the script for the calculator app.

--- script.js
+++ script.js
@@ -3,23 +3,37 @@ const numbers = document.querySelectorAll('#symbol');
 const evaluate = document.querySelector('.evaluate');
 const clear = document.querySelector('.clear');
 const input = document.querySelector('input');
+const removeLastChar = document.querySelector('.delete');
+const operators = ["+", "-", "*", "/", "."];
+let deleteResult = false;
 
 
 symbols.forEach(symbol => {
   symbol.addEventListener('click', () => {
-    input.value += symbol.textContent;
+    let lastOperator = input.value.slice(-1);
+    if(!operators.includes(lastOperator)){
+      input.value += symbol.textContent;
+    }
   });
 });
 
 numbers.forEach(number => {
-    number.addEventListener('click', () => {    
-        input.value += number.textContent;
+    number.addEventListener('click', () => {
+      if(deleteResult && !operators.includes(input.value.slice(-1))){
+        input.value = number.textContent;
+        deleteResult = false;
+      }
+      else{
+          input.value += number.textContent;
+          deleteResult = false;
+      }
     })
 });
 
 evaluate.addEventListener('click', () => {
   try {
     input.value = math.evaluate(input.value);
+    deleteResult = true;
   } catch (err) {
     input.value = "Eroare";
   }
@@ -27,6 +41,11 @@ evaluate.addEventListener('click', () => {
 
 clear.addEventListener('click', () => {
     input.value = "";
-})
+});
 
+removeLastChar.addEventListener('click', () => {
+  if(input.value.length > 0){
+      input.value = input.value.slice(0, - 1);
+  }
+});
 
